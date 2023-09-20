ac2456a73785d20ff5309d412343e95a2a1e255c
Tanvin-Ahmed <tanvinahmedtouhid91@gmail.com>
2023-09-20 11:20:53 +0600

    develop product description page

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -1,5 +1,7 @@
 import { getProduct } from "@/actions/get-product";
 import getProducts from "@/actions/get-products";
+import Gallery from "@/components/gallery";
+import ProductInfo from "@/components/product-info";
 import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
 import React from "react";
@@ -21,8 +23,10 @@ const ProductPage: React.FC<ProductPagePropsType> = async ({ params }) => {
       <Container>
         <div className="px-4 py-10 sm:px-6 lg:px-8">
           <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-            <div>Gallery</div>
-            <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">info</div>
+            <Gallery images={product?.images} />
+            <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
+              <ProductInfo data={product} />
+            </div>
           </div>
           <ProductList title="Related products" items={suggestedProducts} />
         </div>
